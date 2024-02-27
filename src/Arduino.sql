c18ede72e485d6c68192c385e2232343fd2b1f09
rubenvizros <98592976+rubenvizros@users.noreply.github.com>
2024-02-27 12:13:58 +0100

    Update Arduino.sql
    Fix Arduino1 por Arduino

--- src/Arduino.sql
+++ src/Arduino.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `Arduino`
 --
-CREATE DATABASE IF NOT EXISTS `Arduino1` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE DATABASE IF NOT EXISTS `Arduino` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `Arduino`;
 
 -- --------------------------------------------------------
